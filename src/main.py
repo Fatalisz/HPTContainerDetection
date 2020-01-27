c6e01c3b5bfee4e6f26c2dff576b7bac6e0152e0
aurte <aurt1austin@hotmail.comgit config --global user.name Fatalisz>
2020-01-27 16:21:37 +0700

    try draw Rectangle

--- src/main.py
+++ src/main.py
@@ -3,20 +3,20 @@ import myImageUtils
 import numpy as np
 
 # READ IMAGE
-image = cv.imread('../images/gen.jpg', cv.IMREAD_GRAYSCALE)
+image = cv.imread('../images/gen.jpg')
+imCopy = image.copy()
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 # IMAGE THRESHOLD
-threshImg = myImageUtils.getThresholdedImage(image)
-
+threshImg = myImageUtils.getThresholdedImage(gray)
 # CONTOUR IMAGE
-# contours, hierarchy = cv.findContours(threshImg, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-# for i in range(len(contours)):
-#     cv.drawContours(threshImg, contours, i, (0, 255, 0), 3)
+contours, hierarchy = cv.findContours(threshImg, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# Iterate through each contour
+for c in contours:
+    x, y, w, h = cv.boundingRect(c)
+    cv.rectangle(imCopy, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-# FIND BLOB WITH SIMPLE BLOB
-# detector = cv.SimpleBlobDetector()
-# keypoints = detector.detect(threshImg)
-# im_with_keypoints = cv.drawKeypoints(threshImg, keypoints, np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-cv.namedWindow('image', cv.WINDOW_NORMAL)
-cv.imshow('image', threshImg)
+cv.imshow('image with contour', imCopy)
+cv.imshow('image origin', image)
+cv.imshow('image threshold', threshImg)
 cv.waitKey(0)
 cv.destroyAllWindows()
