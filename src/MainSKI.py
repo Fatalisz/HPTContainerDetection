917150dd9af1787da8ee8083bdc4dd50ab7b3d7c
aurte <aurt1austin@hotmail.comgit config --global user.name Fatalisz>
2020-02-03 17:42:50 +0700

    Use scikit-image

--- src/MainSKI.py
+++ src/MainSKI.py
@@ -1,4 +1,5 @@
 import skimage.filters as filter
+import skimage.feature as fea
 from skimage import io
 from skimage.color import rgb2gray
 from skimage.viewer import ImageViewer
@@ -8,35 +9,33 @@ import matplotlib.pyplot as plt
 from skimage import img_as_ubyte
 import matplotlib.patches as mpatches
 from skimage.restoration import denoise_tv_chambolle
-from skimage.morphology import closing, square
 from skimage.segmentation import clear_border
 from scipy import ndimage as ndi
+from skimage import draw
 import numpy as np
 
+# PREPARE INPUT
 image = io.imread('../images/Top/thumbnail_IMG_9517.jpg', True)
 denoise_image = denoise_tv_chambolle(image)
 thresh = filter.threshold_yen(denoise_image)
 binary = image > thresh
-fill_coins = ndi.binary_fill_holes(binary)
-removeSmallPic = morphology.remove_small_objects(fill_coins, 100)
+can = fea.canny(binary)
+filled_img = ndi.binary_fill_holes(can)
+# REMOVE SMALL OBJECT AFTER CANNY
+removeSmallPic = morphology.remove_small_objects(filled_img, 180)
 fig, ax = plt.subplots(figsize=(4, 3))
 ax.imshow(removeSmallPic, cmap=plt.cm.gray)
-ax.set_title('binary')
+ax.set_title('removeSmallPic')
 ax.axis('off')
-bw = closing(removeSmallPic, morphology.disk(11))
-clear_border(bw)
+#### PROCESS
+bw = morphology.closing(removeSmallPic, morphology.rectangle(5,100))
 fig, ax = plt.subplots(figsize=(4, 3))
 ax.imshow(bw, cmap=plt.cm.gray)
-ax.set_title('bw')
-ax.axis('off')
-dila = morphology.dilation(bw, morphology.disk(11))
-con = label(dila)
-
-fig, ax = plt.subplots(figsize=(4, 3))
-ax.imshow(dila, cmap=plt.cm.gray)
-ax.set_title('dilation')
+ax.set_title('closing')
 ax.axis('off')
 
+## LABEL
+con = label(bw)
 fig, ax = plt.subplots()
 ax.imshow(image, cmap=plt.cm.gray)
 for region in regionprops(con):
@@ -44,9 +43,7 @@ for region in regionprops(con):
     if region.area >= 200:
         # draw rectangle around segmented coins
         minr, minc, maxr, maxc = region.bbox
-        bx = (minc, maxc, maxc, minc, minc)
-        by = (minr, minr, maxr, maxr, minr)
-        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+        rect = mpatches.Rectangle((minc-5, minr-5), maxc - minc + 10, maxr - minr + 10,
                                   fill=False, edgecolor='red', linewidth=2)
         ax.add_patch(rect)
         # crop & save image
